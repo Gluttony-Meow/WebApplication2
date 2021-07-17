337023722f4cb30261eab37a4484fb6b5fa06edc
Gluttony-Meow <glutony0910@gmail.com>
2021-07-17 23:07:54 +0700

    Initial commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ArrayList list = new ArrayList();
+
+            CultureInfo[] cinfo = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            foreach (CultureInfo culture in cinfo)
+            {
+                list.Add(String.Format("culture name{0} : Display name{1}", culture.Name, culture.DisplayName));
+            }
+
+            ViewBag.MyList = list; 
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
